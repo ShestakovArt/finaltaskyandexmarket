43fd796cdb28005101d8ffe61d950428391a8946
ArtemSShestakov <temashestakoff@yandex.ru>
2021-07-11 12:12:12 +0300

    111

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"scr/test/resources/"},
+        features = {"src/test/resources"},
         glue = {"steps"},
         tags = "@testcase_1",
         plugin = {"io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"}
